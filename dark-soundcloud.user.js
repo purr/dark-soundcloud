d69fa1cf81b8b78c62a4aec7993eb08cc10e4384
purr <x@x.x>
2025-03-17 21:21:13 +0100

    Update version number to 0.30.1 and improve CSS specificity for comment item styles in dark-soundcloud.css.

--- dark-soundcloud.user.js
+++ dark-soundcloud.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Modern Design and Dark Theme for SoundCloud
-// @version      0.30.0
+// @version      0.30.1
 // @description  A modern design and dark theme for SoundCloud.com, inspired by the SoundCloud Android app.
 // @author       purr
 // @namespace    https://github.com/purr/dark-soundcloud
